c5501237b0d6c548bb5060e4067936a9a63fd074
Marcin-Blok <marcinblok7980@gmail.com>
2021-01-30 19:56:15 +0100

    Dodanie podpakietów calculator i user oraz wykonanie pierwszych testów w klasie Testing Main - jeszcze bez wykorzystania biblioteki Junit

--- kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/TestingMain.java
@@ -1,8 +1,38 @@
 package com.kodilla.testing;
 
+import com.kodilla.testing.calculator.Calculator;
+import com.kodilla.testing.user.SimpleUser;
+
 public class TestingMain {
 
     public static void main(String[] args) {
-        System.out.println("Moduł 6. Wprowadzenie to testowania oprogramowania");
+
+        SimpleUser simpleUser = new SimpleUser("theForumUser");
+        Calculator calculator = new Calculator();
+
+        // Testowanie simpleUser
+        String result = simpleUser.getUsername();
+        if(result.equals("theForumUser")){
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Failed!");
+        }
+
+
+        // Testowanie metod add() i substruct() z klasy Calculator
+        int addingResult = calculator.add(5, 455);
+        int substructResult = calculator.substruct(400,70);
+
+        if(addingResult == 460){
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Test failed!");
+        }
+
+        if(substructResult == 330){
+            System.out.println("Test OK");
+        }else{
+            System.out.println("Test failed!");
+        }
     }
 }
